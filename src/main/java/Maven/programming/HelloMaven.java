45449189d358edf6ec625bf3e79def312a2de9ae
b0b1n <sohaibskious@gmail.com>
2022-09-08 11:30:32 +0200

    project configured

---
+++ src/main/java/Maven/programming/HelloMaven.java
@@ -0,0 +1,13 @@
+package Maven.programming;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloMaven
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven!" );
+    }
+}
---
+++ src/test/java/Maven/programming/HelloMavenTest.java
@@ -0,0 +1,20 @@
+package Maven.programming;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloMavenTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
