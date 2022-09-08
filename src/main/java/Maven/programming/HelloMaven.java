ace075547b33e2b124bac4c79d81874339cdb1f5
b0b1n <sohaibskious@gmail.com>
2022-09-08 11:51:06 +0200

    maven configures

--- src/main/java/Maven/programming/HelloMaven.java
+++ src/main/java/Maven/programming/HelloMaven.java
@@ -6,6 +6,7 @@ package Maven.programming;
  */
 public class HelloMaven
 {
+    private final Logger
     public static void main( String[] args )
     {
         System.out.println( "Hello Maven!" );
